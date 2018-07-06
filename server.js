7a6338b82679e9e4f970d433a4bf168d7304f414
Sam Yan <yzenithcorp@gmail.com>
2018-07-05 22:44:48 -0500

    fix route models

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 // we import the ShoppingList model, which we'll
 // interact with in our GET endpoint
 const {ShoppingList} = require('./models');
-const {Recipes} = require('./modules');
+const {Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
